0cdd55fb1b4fcb3b33dab5808e202f66cf6b35c9
Robert Kovacs <19787665+codepumper@users.noreply.github.com>
2024-10-13 19:45:13 +0000

    add github workflow

--- create_deployment.py
+++ create_deployment.py
@@ -1,13 +1,28 @@
 from prefect import flow
+import os
 
-SOURCE_REPO="https://github.com/codepumper/workflows.git"
+SOURCE_REPO = "https://github.com/codepumper/workflows.git"
 
-if __name__ == "__main__":
+@flow
+def deploy_eodhd_pipeline():
+    eodhd_api_key = os.getenv('EODHD_API_KEY')
+    mother_duck_token = os.getenv('MOTHER_DUCK_TOKEN')
+    if not eodhd_api_key or not mother_duck_token:
+        raise ValueError("Error: environment variable is not set.")
+
     flow.from_source(
         source=SOURCE_REPO,
         entrypoint="eodhd_pipeline.py:run_eodhd_data_pipeline",
     ).deploy(
         name="eodhd_pipeline",
         work_pool_name="data-pipeline-work-pool",
+        job_variables={
+            "env":
+                {"EODHD_API_KEY": eodhd_api_key},
+                {"MOTHER_DUCK_TOKEN": mother_duck_token}
+        },
         cron="5 0 * * *",
     )
+
+if __name__ == "__main__":
+    deploy_eodhd_pipeline()
